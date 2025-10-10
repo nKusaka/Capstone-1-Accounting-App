e2fdeb640d6e68f80ef2f51f9ad537eaa0f6f5ad
Nik <nikkusaka@gmail.com>
2025-10-10 11:52:17 -0700

    -Created first version of the homescreen

---
+++ src/main/java/com/accounting/HomeScreen.java
@@ -0,0 +1,23 @@
+package com.accounting;
+import java.util.*;
+import java.io.*;
+
+public class HomeScreen {
+    public static void homeScreen() {
+        String userInput = "";
+        Scanner read = new Scanner(System.in);
+        boolean firstLaunch = true;
+        do {
+
+            if (firstLaunch) {
+                System.out.println("==============Home Screen=====================\n"
+                        + "               Press D to add a deposit                \n"
+                        + "               Press P to make a payment               \n"
+                        + "          Press L to display the Ledger screen         \n"
+                        + "=======================================================");
+                firstLaunch = false;
+            }
+            userInput = read.nextLine();
+        } while (!userInput.equalsIgnoreCase("x"));
+    }
+}
---
+++ src/main/java/com/accounting/Main.java
@@ -0,0 +1,11 @@
+package com.accounting;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        HomeScreen.homeScreen();
+    }
+
+}
