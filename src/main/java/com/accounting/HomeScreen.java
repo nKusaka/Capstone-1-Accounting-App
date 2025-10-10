795d46412c1eae94362d4544b5ac53b6056081b1
Nik <nikkusaka@gmail.com>
2025-10-10 12:27:12 -0700

    -Added while loop to hand exception when user does not enter a correct input.

--- src/main/java/com/accounting/HomeScreen.java
+++ src/main/java/com/accounting/HomeScreen.java
@@ -4,22 +4,37 @@ import java.io.*;
 
 public class HomeScreen {
     public static void homeScreen() {
+
         String userInput = "";
         Scanner read = new Scanner(System.in);
-        boolean firstLaunch = true;
-        do {
 
-            if (firstLaunch) {
-                System.out.println("""
+        do {
+            System.out.printf("""
                                   |=====================Home Screen=====================|
                                   |              Press D to add a deposit               |
                                   |              Press P to make a payment              |
                                   |         Press L to display the Ledger screen        |
                                   |           Press X to exit the application           |
-                                  |=====================================================|""");
-                firstLaunch = false;
-            }
+                                  |=====================================================|
+                                  Enter command:""");
+
             userInput = read.nextLine();
+
+            while (!userInput.equalsIgnoreCase("d") && !userInput.equalsIgnoreCase("p") && !userInput.equalsIgnoreCase("l") && !userInput.equalsIgnoreCase("x")) {
+                System.out.printf("Please enter a valid input: ");
+                userInput = read.nextLine();
+            }
+         /*   switch (userInput.toLowerCase()) {
+                case "d":
+                    addDeposit();
+                    break;
+                case "p":
+                    makePayment();
+                    break;
+                case "l":
+                    displayLedger();
+                    break;
+            }*/
         } while (!userInput.equalsIgnoreCase("x"));
     }
 }
